5f9713a9ba7ddfba6a7ed231de1d99e2d4ffd8d5
MrAnyx <1ptitigre@gmail.com>
2023-11-20 22:08:51 +0100

    feat(core) reset version and added readme

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
-import { App, Notice, Plugin, PluginSettingTab, Setting, TFile, TFolder, Vault } from 'obsidian';
+import { App, Notice, Plugin, PluginSettingTab, Setting } from "obsidian";
 import { getAPI } from "obsidian-dataview";
-import { FolderInputSuggester } from './Settings/FolderInputSuggester';
+import { FolderInputSuggester } from "./Settings/FolderInputSuggester";
 
 interface PluginSettings {
 	defaultLocation: string;
@@ -8,13 +8,13 @@ interface PluginSettings {
 
 const DEFAULT_SETTINGS: PluginSettings = {
 	defaultLocation: "",
-}
+};
 
 export default class NoteBatcherPlugin extends Plugin {
 	settings: PluginSettings;
 
 	getExtension(path: string): string | undefined {
-		if(path.contains(".")) {
+		if (path.contains(".")) {
 			return path.split(".").last();
 		}
 
@@ -22,20 +22,20 @@ export default class NoteBatcherPlugin extends Plugin {
 	}
 
 	isDataviewEnabled(): boolean {
-		return !!getAPI(this.app)
+		return !!getAPI(this.app);
 	}
 
 	isFolderPathValid(path: string): boolean {
-		return !!this.app.vault.getAbstractFileByPath(!!path ? path : "/");
+		return !!this.app.vault.getAbstractFileByPath(path ? path : "/");
 	}
 
 	getDefaultFolder(): string | undefined {
-		let rootPath = this.settings.defaultLocation;
+		const rootPath = this.settings.defaultLocation;
 
 		if (this.isFolderPathValid(rootPath)) {
 			return rootPath;
 		} else {
-			return
+			return;
 		}
 	}
 
@@ -48,15 +48,17 @@ export default class NoteBatcherPlugin extends Plugin {
 		const dv = getAPI(this.app);
 		const defaultFolder = this.getDefaultFolder();
 
-		if(defaultFolder === undefined) {
-			new Notice(`Default location \"${this.settings.defaultLocation}\" doesn't exist. You must modify the settings.`);
+		if (defaultFolder === undefined) {
+			new Notice(
+				`Default location "${this.settings.defaultLocation}" doesn't exist. You must modify the settings.`
+			);
 			return;
 		}
 
 		dv.pages().forEach((p: any) => {
 			p.file.outlinks.values.forEach((o: any) => {
-				const fileExist = !!dv.page(o.path)?.files
-				const hasExtension = !!this.getExtension(o.path)
+				const fileExist = !!dv.page(o.path)?.files;
+				const hasExtension = !!this.getExtension(o.path);
 
 				if (!fileExist && !hasExtension) {
 					this.app.vault.create(`${defaultFolder}/${o.path}.md`, "");
@@ -68,23 +70,31 @@ export default class NoteBatcherPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		const ribbonIconEl = this.addRibbonIcon('link', 'Create unresolved notes', (evt: MouseEvent) => {
-			this.batchCreate();
-		});
-
-		this.addCommand({
-			id: 'create-unresolved-notes',
-			name: 'Create unresolved notes',
-			callback: () => {
+		this.addRibbonIcon(
+			"link",
+			"Create unresolved notes",
+			(evt: MouseEvent) => {
 				this.batchCreate();
 			}
+		);
+
+		this.addCommand({
+			id: "create-unresolved-notes",
+			name: "Create unresolved notes",
+			callback: () => {
+				this.batchCreate();
+			},
 		});
 
 		this.addSettingTab(new SettingTab(this.app, this));
 	}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
@@ -105,11 +115,13 @@ class SettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(this.containerEl)
-            .setName("New note location")
-            .setDesc("Folder that will contain all new notes. Empty value is equivalent to the vault root.")
-            .addSearch((cb) => {
+			.setName("New note location")
+			.setDesc(
+				"Folder that will contain all new notes. Empty value is equivalent to the vault root."
+			)
+			.addSearch((cb) => {
 				new FolderInputSuggester(this.app, cb.inputEl);
-                cb.setPlaceholder("Example: folder1/folder2")
+				cb.setPlaceholder("Example: folder1/folder2")
 					.setValue(this.plugin.settings.defaultLocation)
 					.onChange((newFolder) => {
 						this.plugin.settings.defaultLocation = newFolder;
@@ -117,8 +129,8 @@ class SettingTab extends PluginSettingTab {
 					});
 			});
 
-			new Setting(this.containerEl)
-            	.setDesc("If you use a template plugin like Templater with folder templates, the folder template will be applied.");
-            
+		new Setting(this.containerEl).setDesc(
+			"If you use a template plugin like Templater with folder templates, the folder template will be applied."
+		);
 	}
 }
